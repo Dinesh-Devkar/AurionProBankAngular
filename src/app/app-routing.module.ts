876a79ef4198f3741ac55513ee0ebd9d013575bf
unknown <aniketdonode14@gmail.com>
2022-06-07 19:26:38 +0530

    registrartion Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './home/landing/landing.component';
+import {RegisterComponent} from './home/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:"register",component:RegisterComponent},
+                        {path:"landing",component:LandingComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
