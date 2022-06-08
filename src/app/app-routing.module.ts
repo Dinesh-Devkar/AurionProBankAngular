8bdf5969960d17bc39f5d0b64fa56afe71ad1dfb
unknown <aniketdonode14@gmail.com>
2022-06-08 18:58:26 +0530

    Style Improved

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CustomerDashboardComponent } from './home/customer-dashboard/customer-dashboard.component';
 import { LandingComponent } from './home/landing/landing.component';
+import { LoginformComponent } from './home/loginform/loginform.component';
 import {RegisterComponent} from './home/register/register.component';
 
 const routes: Routes = [{path:"register",component:RegisterComponent},
                         {path:"landing",component:LandingComponent},
                         {path:"",component:LandingComponent},
                         {path:"customerDashboard",component:CustomerDashboardComponent},
+                        {path:"login",component:LoginformComponent}
                         
 ];
 
